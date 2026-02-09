8c7b4a0c0d78c3aa15e7c444a5417c93390d4845
kish <simerkish@gmail.com>
2026-02-09 15:43:35 +0400

    chords seqs generation with drums

--- Services/AudioGenerator/AudioGenerator.cs
+++ Services/AudioGenerator/AudioGenerator.cs
@@ -1,5 +1,4 @@
-﻿using ITask5.Models;
-using ITask5.Services.DataGenerator;
+﻿using NAudio.Mixer;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 
@@ -12,24 +11,55 @@ public class AudioGenerator : IAudioGenerator
         return GenerateRandomMusic(seed, duration);
     }
 
-    private static readonly float[] CMajor = {
-        261.63f, 293.66f, 329.63f, 349.23f, 392.00f, 440.00f, 493.88f, 523.25f
-    };
     private const float SemiTone = 1.0594631f;
+    private static readonly float[] CMajor = {
+        261.63f, 293.66f, 329.63f, 349.23f, 392.00f, 440.00f, 493.88f
+    };
+    private static readonly int[] StandardChordStages = {
+        0, 2, 4
+    };
 
-    public byte[] GenerateRandomMusic(int seed, int durationSeconds)
+    private static readonly List<int[]> PopularMinorChordsProgressions =
+    [
+        [1, 6, 7],
+        [1, 4, 7],
+        [1, 4, 5],
+        [1, 6, 3, 7],
+        [2, 5, 1],
+        [1, 4, 5, 1],
+        [6, 7, 1, 1],
+        [1, 7, 6, 7],
+        [1, 4, 1]
+    ];
+    private static readonly List<int[]> PopularMajorChordsProgressions =
+    [
+        [1, 4, 5],
+        [1, 6, 2, 5],
+        [1, 3, 4, 5],
+        [1, 6, 4, 5],
+        [1, 5, 6, 4],
+        [1, 4, 1, 5],
+        [1, 4, 2, 5],
+        [1, 4, 6, 5],
+        [2, 5, 1]
+    ];
+    
+    private byte[] GenerateRandomMusic(int seed, int durationSeconds)
     {
         Random random = new Random(seed);
         int sampleRate = 44100;
         int channels = 2;
         int totalSamples = sampleRate * durationSeconds;
+        int bpm = random.Next(70, 180);
+
+        float[] notes = CreateKey(random, out bool isMajor);
+        int[] progression = GetRandomProgression(random, isMajor);
+        List<List<float>> chordsFrequencies = GetChordFrequencies(notes, progression);
         
-        float[] notes = new float[CMajor.Length];
-        Array.Copy(CMajor, notes, CMajor.Length);
-        KeyShift(notes, random.Next(-5, 5));
-        MajorMinorKey(notes, random.Next() % 2);
+        int samplesPerTact = sampleRate * 240 / bpm;
+        int samplesPerDrum1 = samplesPerTact / 2;
+        int samplesPerDrum2 = samplesPerTact / 4;
         
-        int samplesPerNote = sampleRate / 2; 
         using var ms = new MemoryStream();
         var waveFormat = new WaveFormat(sampleRate, 16, channels);
     
@@ -37,24 +67,28 @@ public class AudioGenerator : IAudioGenerator
         {
             for (int i = 0; i < totalSamples; i++)
             {
-                int noteIndex = (i / samplesPerNote) % notes.Length;
-                float frequency = notes[noteIndex];
-            
-                double time = (double)i / sampleRate;
-                float sample = (float)Math.Sin(2 * Math.PI * frequency * time);
+                int currentNote = (i / samplesPerTact) % progression.Length;
+                double timeSinceTact = (double)(i % samplesPerTact) / sampleRate;
+                double timeSinceDrum1 = (double)(i % samplesPerDrum1) / sampleRate;
+                double timeSinceDrum2 = (double)(i % samplesPerDrum2) / sampleRate;
                 
-                int positionInNote = i % samplesPerNote;
+                float sample = GetSample(timeSinceTact, chordsFrequencies[currentNote]) * 0.05f;
+                sample = ApplyOverdrive(sample, 10f, 1f);
+                
+                int positionInNote = i % samplesPerTact;
                 float envelope = 1.0f;
-                int fadeSamples = 2000;
+                int fadeSamples = 1000;
             
                 if (positionInNote < fadeSamples) 
                     envelope = positionInNote / (float)fadeSamples;
-                else if (positionInNote > samplesPerNote - fadeSamples) 
-                    envelope = (samplesPerNote - positionInNote) / (float)fadeSamples;
+                else if (positionInNote > samplesPerTact - fadeSamples) 
+                    envelope = (samplesPerTact - positionInNote) / (float)fadeSamples;
             
                 sample *= envelope * 0.3f;
-            
-                // Stereo
+                
+                sample += GetKickSample(timeSinceDrum1);
+                sample += GetHiHatSample(timeSinceDrum2, random);
+                
                 writer.WriteSample(sample);
                 writer.WriteSample(sample);
             }
@@ -73,14 +107,82 @@ public class AudioGenerator : IAudioGenerator
         return notes;
     }
 
-    private float[] MajorMinorKey(float[] notes, int value)
+    private float[] MinorFromMajor(float[] notes)
     {
-        if (value > 0)
-        {
-            notes[2] /= SemiTone;
-            notes[5] /= SemiTone;
-            notes[6] /= SemiTone;
-        }
+        notes[2] /= SemiTone;
+        notes[5] /= SemiTone;
+        notes[6] /= SemiTone;
         return notes;
+    }
+
+    private float[] ExtendKey(float[] notes)
+    {
+        float[] extendedNotes = new float[notes.Length * 2];
+        Array.Copy(notes, extendedNotes, notes.Length);
+        for (int i = 0; i < notes.Length; i++)
+        {
+            extendedNotes[i + notes.Length] = notes[i] * 2;
+        }
+        return extendedNotes;
+    }
+
+    private float GetSample(double time, List<float> frequenciesPlaying)
+    {
+        float sample = 0;
+        foreach (float frequency in frequenciesPlaying)
+        {
+            sample += (float)Math.Sin(2 * Math.PI * frequency * time);
+        }
+        return sample;
+    }
+
+    private List<List<float>> GetChordFrequencies(float[] notes, int[] progression)
+    {
+        List<List<float>> chords = new();
+        for (int i = 0; i < progression.Length; i++)
+        {
+            chords.Add(new List<float>());
+            for (int j = 0; j < StandardChordStages.Length; j++)
+            {
+                chords[i].Add(notes[progression[i] + StandardChordStages[j]]);
+            }
+        }
+        return chords;
+    }
+
+    private float[] CreateKey(Random random, out bool isMajor)
+    {
+        float[] notes = new float[CMajor.Length];
+        Array.Copy(CMajor, notes, CMajor.Length);
+        KeyShift(notes, random.Next(-5, 5));
+        isMajor = random.Next() % 1 == 0;
+        if (!isMajor) MinorFromMajor(notes);
+        return ExtendKey(notes);
+    }
+
+    private int[] GetRandomProgression(Random random, bool isMajor)
+    {
+        List<int[]> progressions = isMajor ? PopularMajorChordsProgressions : PopularMinorChordsProgressions;
+        return progressions[random.Next(progressions.Count)];
+    }
+    
+    float GetKickSample(double time)
+    {
+        double freq = 123 / (time + 1);  
+        double amp = Math.Exp(-3.0 * time);
+        return (float)(Math.Sin(2.0 * Math.PI * freq * time) * 0.5f * amp);
+    }
+    
+    float GetHiHatSample(double time, Random random)
+    {
+        float amp = (float)Math.Exp(-4.0 * time);
+        return (random.NextSingle() - 0.5f) * 0.2f * amp;
+    }
+    
+    public static float ApplyOverdrive(float input, float drive, float mix)
+    {
+        float amplified = input * drive;
+        float distorted = (float)Math.Tanh(amplified);
+        return input * (1 - mix) + distorted * mix;
     }
 }
